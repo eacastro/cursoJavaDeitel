412d56b670bb952139866e4f71bfce08fa62e4a2
Edisson Castro <edissoncastro.11@hotmail.com>
2020-02-11 14:03:02 -0500

    Declaro la clase PruebaEmpleado para demostrar el acceso a campos static de la clase Empleado mediante el uso de una variable de tipo referencia y mediante la llamada a su clase a través de la notación de punto

---
+++ src/miembrosStatic/PruebaEmpleado.java
@@ -0,0 +1,26 @@
+package miembrosStatic;
+
+public class PruebaEmpleado 
+{
+
+	public static void main( String args[] )
+	{
+		
+		// Muestra que la cuenta es 0 antes de crear Empleados
+		System.out.printf( "Empleados antes de instanciar: %d\n", 
+				Empleado.obtenerCuenta() );
+		
+		// Crea dos Empleados; la cuenta debe ser 2
+		Empleado e1 = new Empleado( "Susan", "Baker" );
+		Empleado e2 = new Empleado( "Bob", "Blue" );
+		
+		// Muestra que la cuenta es 2 después de crear dos Empleados
+		System.out.println( "\nEmpleados después de instanciar: " );
+		System.out.printf( "mediante e1.obtenerCuenta(): %d\n", e1.obtenerCuenta() );
+		System.out.printf( "mediante e2.obtenerCuenta(): %d\n", e2.obtenerCuenta() );
+		System.out.printf( "mediante Empleado.obtenerCuenta(): %d\n", 
+				Empleado.obtenerCuenta() );
+		
+	} // Fin main
+	
+} // Fin PruebaEmpleado
