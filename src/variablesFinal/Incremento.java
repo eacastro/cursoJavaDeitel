cd1e98162ca2b5acb3d37316b5f5416cf3072626
Edisson Castro <edissoncastro.11@hotmail.com>
2020-02-22 22:04:54 -0500

    Genero el paquete variablesFinal donde se incluirán las clases para ilustrar las variables de instancia final (o constantes) dentro de Java

---
+++ src/variablesFinal/Incremento.java
@@ -0,0 +1,18 @@
+package variablesFinal;
+// Variable de instancia final en una clase
+
+public class Incremento 
+{
+	
+	private int total = 0; // El total de todos los incrementos
+	private final int INCREMENTO; // Variable constante (sin inicializar)
+	
+	// El constructor inicializ
+	public Incremento( int valorIncremento ) 
+	{
+		
+		
+		
+	} // Fin del constructor Incremento
+	
+} // Fin de la clase Incremento
