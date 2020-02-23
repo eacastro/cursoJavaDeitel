9946e8c089013d57216d3eb9548d104ff6d80133
Edisson Castro <edissoncastro.11@hotmail.com>
2020-02-23 15:49:33 -0500

    Creo el paquete accesoPaquetes y la clase PruebaDatosPaquete

---
+++ src/accesoPaquetes/PruebaDatosPaquete.java
@@ -0,0 +1,10 @@
+// Los miembros con acceso a nivel de paquete de una clase son accesibles
+// para las demás clases en el mismo paquete
+package accesoPaquetes;
+
+public class PruebaDatosPaquete 
+{
+	
+	
+	
+} // Fin de la clase PruebaDatosPaquete
