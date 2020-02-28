b3604aaef95d82430d6d7befb62308e0fcbba50b
Edisson Castro <edissoncastro.11@hotmail.com>
2020-02-28 14:01:06 -0500

    Creo la clase EmpleadoBaseMasComision dentro del paquete ejercicio9_3

---
+++ src/ejercicio9_3/EmpleadoBaseMasComision.java
@@ -0,0 +1,16 @@
+package ejercicio9_3;
+import herencia.EmpleadoPorComision;
+/*
+ * Muchos programas escritos con herencia podrían escribirse mediante
+ * la composición, y viceversa. Vuelva a escribir las clases EmpleadoBaseMasComision
+ * de la jerarquía EmpleadoPorComision-EmpleadoBaseMasComision para usar la
+ * composición en vez de la herencia *
+ */
+public class EmpleadoBaseMasComision 
+{
+	
+	private double salarioBase; // Salario base por semana
+	
+	
+
+}
