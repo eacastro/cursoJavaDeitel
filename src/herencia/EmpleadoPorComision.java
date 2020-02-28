fc7d038be8b46dc51bf067de6240818b7a58c8ac
Edisson Castro <edissoncastro.11@hotmail.com>
2020-02-28 11:55:17 -0500

    Añado variables de instancia, constructor y un método setter

--- src/herencia/EmpleadoPorComision.java
+++ src/herencia/EmpleadoPorComision.java
@@ -1,13 +1,37 @@
 package herencia;
 
-public class EmpleadoPorComision
+public class EmpleadoPorComision extends Object // Hacemos explícita la herencia con Object
 {
 	
 	private String primerNombre;
 	private String apellidoPaterno;
 	private String numeroSeguroSocial;
-	private double ventasBrutas;
-	private double tarifaComision;
+	private double ventasBrutas; 		// Ventas semanales totales
+	private double tarifaComision;		// Porcentaje de comisión
+	
+	public EmpleadoPorComision( String nombre, String apellido, String nss,
+			double ventas, double tarifa ) 
+	{
+		
+		// La llamada implícita al constructor del objeto
+		// de la superclase ocurre aquí
+		primerNombre = nombre;
+		apellidoPaterno = apellido;
+		numeroSeguroSocial = nss;
+		establecerVentasBrutas( ventas ); 	// Valida y almacena las ventas brutas
+		establecerTarifaComision( tarifa ); // Valida y almacena la tarifa de comisión
+		
+	} // Fin del método constructor
+	
+	public void establecerPrimerNombre( String nombre ) 
+	{
+		
+		
+		primerNombre = nombre;
+		
+	} // Fin del método establecerPrimerNombre
+	
+	
 	
 	
 	
