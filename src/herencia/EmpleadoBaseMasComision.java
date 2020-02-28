339623180af21cc463f69bdd837026fa4af50203
Edisson Castro <edissoncastro.11@hotmail.com>
2020-02-28 13:44:28 -0500

    Implemento el método constructor de la clase EmpleadoBaseMasComision

--- src/herencia/EmpleadoBaseMasComision.java
+++ src/herencia/EmpleadoBaseMasComision.java
@@ -3,7 +3,16 @@ package herencia;
 public class EmpleadoBaseMasComision extends EmpleadoPorComision
 {
 	
+	private double salarioBase; // Salario base por semana
 	
+	public EmpleadoBaseMasComision( String nombre, String apellido,
+			String nss, double ventas, double tarifa, double salario )
+	{
+		
+		super( nombre, apellido, nss, ventas, tarifa );
+		establecerSalarioBase( salario );
+		
+	} // Fin del método constructor
 	
 	
 	
